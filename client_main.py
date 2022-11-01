a87b134a1017429a17b6f34c657ff68e66e28629
lucabln05 <luca.bellon@icloud.com>
2022-11-01 18:24:51 +0100

    Add Cache System for Session_Key

--- client_main.py
+++ client_main.py
@@ -8,14 +8,21 @@ port = 81
 ClientMultiSocket.connect((host, port))
 
 
-res = ClientMultiSocket.recv(2048)
-
 while True:
+
     user_name = input('User: ')
     password = input('Password: ')
     login_send = f'{user_name},{password}'
     ClientMultiSocket.send(str.encode(login_send))
     res = ClientMultiSocket.recv(2048)
-    sessionkey = res.decode('utf-8')
-    print(sessionkey)
+    login_server_answer = res.decode('utf-8')
+    user_msg, session_token = login_server_answer.split(':')
+    print(user_msg)
+    if user_msg == "Login successful":
+        ClientMultiSocket.send(str.encode(session_token))
+        
+           
+        
+    
+    
 ClientMultiSocket.close() 
--- server_main.py
+++ server_main.py
@@ -1,13 +1,16 @@
 from audioop import mul
+from email.utils import localtime
 from random import random, randrange
 import socket
-from _thread import *  
+from _thread import * 
+from datetime import datetime       #https://www.programiz.com/python-programming/datetime/current-time
 
 
+#get local ip adress and start in port 81 hosting of tcp server
 ServerSideSocket = socket.socket()
 server_ipaddr = socket.gethostbyname(socket.gethostname())
 server_port = 81
-max_multi_connections = 10
+max_multi_connections = 10              #connections at the same time 
 try:
     ServerSideSocket.bind((server_ipaddr, server_port))
 except Exception as err:
@@ -16,15 +19,25 @@ ServerSideSocket.listen(max_multi_connections)       #how many clients can conne
 
 print(f'Server online on {server_ipaddr}:{server_port} with max {max_multi_connections} connections.')
 
+#reset the session_database
+# code to delete entire data 
+# but not the file, it is in
+# open file 
+f = open("Database/session_database.txt", "r+") 
+# absolute file positioning
+f.seek(0) 
+# to erase all data 
+f.truncate() 
+
 
 
 #login session for a client
 def multi_threaded_client(client_connection):
-    client_connection.send(str.encode('Server is working: '))
     while True:
+        #server wait for login data from client
         data = client_connection.recv(2048)
         client_msg = data.decode('utf-8')
-        print(f'{client_address}>> {client_msg}')
+        print(f'{client_address}:{datetime.now().strftime("%H:%M:%S")}:>> {client_msg}')       #server log with client ip, time, login data
         
         #split incoming username and password
         try:
@@ -38,35 +51,48 @@ def multi_threaded_client(client_connection):
         #read database and check if user and password are in there
         file = open('Database/user_database.txt', 'r')      #open user_database file in read only
         login_info = {}                                     #define a pre list for the database content
+        
+        
         #splite username and password in each line and add them in login_info list
         for line in file:
             database_user, database_password = line.split(',')
             database_password = database_password.strip()
             login_info[database_user] = database_password
+        file.close
+        
+        
     
         #check if user_input is in user_database
-        if  user_client_request in login_info.keys():          
-            if (login_info[user_client_request] == password_client_request):
+        if  user_client_request in login_info.keys():   
+                   
+            if (login_info[user_client_request] == password_client_request):        #check if password is from this user
+
                 #Send session key for secure requests
                 session_key = f"{randrange(10000000000, 99999999999)}"
-                client_connection.sendall(str.encode(session_key))
+                client_connection.sendall(str.encode(f"Login successful:{session_key}"))
+                #save sessionkey in cache file that client can authenicate
+                with open('Database/session_database.txt', 'a') as add_last_line:
+                    add_last_line.write(f'{user_client_request}, {session_key}\n')
+                add_last_line.close()
+                
             else:
-                response = "Login Error"
+                response = "Login Error:Wrong Password"
                 client_connection.sendall(str.encode(response))
+                
         else:
-            response = "Login Error"
+            response = "Login Error:Wrong Username"
             client_connection.sendall(str.encode(response))
-        
-        
+            
         if not data:
             break
+        
     client_connection.close()
 
 
 #if new client connect start a client login session
 while True:
     client_connection, client_address = ServerSideSocket.accept()
-    print(f'Connected from {client_address}')
+    print(f'Connected from {client_address} at {datetime.now().strftime("%H:%M:%S")}')
     start_new_thread(multi_threaded_client, (client_connection, ))
     
 ServerSideSocket.close()
